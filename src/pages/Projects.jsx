5e2d7d45e8e093df96a45a3ac1be7b1ebd91a483
aljaz-ferenc <aljaz.ferenc@protonmail.com>
2023-12-23 06:48:42 +0100

    smooth scroll

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,8 +2,25 @@ import "./Projects.scss";
 import projects from "../data/projects.json";
 import ProjectCard from "../components/ProjectCard";
 import { useDocumentTitle } from "../functions/hooks";
+import Lenis from '@studio-freight/lenis'
+import { useEffect } from "react";
 
 export default function Projects() {
+
+  useEffect(() => {
+    const lenis = new Lenis({duration: 1, easing:(t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))})
+
+    lenis.on('scroll', (e) => {
+      console.log(e)
+    })
+    
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    
+    requestAnimationFrame(raf)
+  }, [])
   useDocumentTitle("Projects");
 
   return (
