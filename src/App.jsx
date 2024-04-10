e72e109acd83757a68a477478487530deaf84f34
Aljaž Ferenc <aljaz.ferenc@protonmail.com>
2024-04-10 16:52:13 +0200

    show link to new page

--- src/App.jsx
+++ src/App.jsx
@@ -29,9 +29,15 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <div className="app">
+    <div className="app" style={{height:'100%'}}>
       <div className="app__background"></div>
-      <RouterProvider router={router} />
+      {/* 
+      <RouterProvider router={router} /> */}
+      <div style={{display:'flex', flexDirection:'column', gap:'1rem', alignItems:'center',  position:'absolute', top:'50%', left:'50%', transform:'translate(-50%, -50%)'}}>
+      <h1>I have moved!</h1>
+      <p>The new version of my page is available here:</p>
+      <a style={{color: 'white', textDecoration:'none', backgroundColor:'steelblue', padding:'0.3rem 1rem', borderRadius:'10px'}} href="https://portfolio-v3-nine-lac.vercel.app/">Portfolio v3.0</a>
+      </div>
     </div>
   );
 }
